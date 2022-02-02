8285f744b3b2e75a8f12ff0780bac93119243d80
keithgaines <93451842+keithgaines@users.noreply.github.com>
2022-02-02 13:36:06 -0600

    infinite loop fix/formatting/comments
    fixed a bug causing an infinite loop
    edited formatting for readability
    added comments for clarity

--- main.py
+++ main.py
@@ -2,46 +2,48 @@ import random
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
-
-chosen_number = int(random.choice(range(1, 100)))
-successful_guess = False
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
+# chooses a random number from a range of 1 - 100 cast as an integer
+chosen_number = int(random.choice(range(1, 100)))
+
+# if statement that happens based on chosen difficulty
 if difficulty == "easy":
     attempts = 10
     print("You have 10 attempts remaining to guess the number.")
     guess = int(input("Make a guess: "))
+    
     while attempts > 0:
         if guess > chosen_number:
             guess = int(input("Too high. Guess again: "))
             attempts -= 1
-            input("Guess again: ")
             
         if guess < chosen_number:
             guess = int(input("Too low. Guess again: "))
             attempts -= 1
             
-        else:
+        if guess == chosen_number:
             print("You guessed it")
-            successful_guess == True
+            attempts = 0
     
 else: 
     attempts = 5
-    print("You have 5 attempts remaining to guess the number.")
+    print("You have 5 remaining to guess the number.")
     guess = int(input("Make a guess: "))
+    
     while attempts > 0:
         if guess > chosen_number:
             guess = int(input("Too high. Guess again: "))
             attempts -= 1
-            input("Guess again: ")
-
+            
         if guess < chosen_number:
             guess = int(input("Too low. Guess again: "))
             attempts -= 1
             
-        else:
+        if guess == chosen_number:
             print("You guessed it")
-            successful_guess == True
+            attempts = 0
+
 if attempts == 0:
-    print(f"You are out of attempts. The correct number was {chosen_number}. Game over")
+    print(f"The correct number was {chosen_number}. Game over")
 
