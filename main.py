d5006cd938c194dd2284347c2f143c13d95ad38e
keithgaines <93451842+keithgaines@users.noreply.github.com>
2022-02-03 12:45:44 -0600

    added comments for clarity
    formatting
    added comments

--- main.py
+++ main.py
@@ -2,26 +2,22 @@ from game_data import data
 import random
 from art import logo, vs
 
-# selects random account from data in game_data
 def get_random_account():
+  """selects random account from data in game_data"""
   return random.choice(data)
 
-# assigns name/description/country from data to variables of the same name
-# returns the information in a printable form
 def format_data(account):
+  """assigns name/description/country from data to variables of the same name, and prints the information"""
   name = account["name"]
   description = account["description"]
   country = account["country"]
   return f"{name}, a {description}, from {country}"
 
 def check_answer(guess, a_followers, b_followers):
-  """Checks followers against user's guess 
-  and returns True if they got it right.
-  Or False if they got it wrong.""" 
   if a_followers > b_followers:
-    return guess == "a"
+    return guess == "a" # returns True because double equal sign checks if variable is equal to value. if A has more followers than b, and the user guessed a, then  guess (a) is equal to "a" and the statement returns true
   else:
-    return guess == "b"
+    return guess == "b" # if b_followers > a_followers and guess == b, then the statement returns true because guess is equal to "b". otherwise, the statement returns false
 
 
 def game():
@@ -36,7 +32,6 @@ def game():
   while not game_over:
     account_a = account_b
     account_b = get_random_account()
-
     while account_a == account_b:
       account_b = get_random_account()
 
@@ -48,10 +43,12 @@ def game():
     # assigns follower count value from dictionary to follower_count variable for accounts a and b
     a_follower_count = account_a["follower_count"]
     b_follower_count = account_b["follower_count"]
+
+    # checks answer and assigns the returned True or False value to the variable is_correct
     is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
     print(logo)
-    if is_correct:
+    if is_correct: # if is_correct is true
       score += 1
       print(f"Correct! Current score: {score}.")
     else:
@@ -60,4 +57,3 @@ def game():
       input("Press any key to end program...")
 
 game()
-
