939a51af03a29dd134107026d0fda28e532d8d1b
keithgaines <93451842+keithgaines@users.noreply.github.com>
2022-03-02 11:06:23 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+import turtle
+
+# initializes screen
+screen = turtle.Screen()
+screen.setup(width=900, height=900, startx=0, starty=0)
+
+# turtles basic settings
+turtle.colormode(255)
+jim = turtle.Turtle()  # initializes the turtle named Jim
+
+
+def move_forward():
+    jim.pendown()
+    jim.pencolor("black")
+    jim.forward(10)
+
+
+def move_backwards():
+    jim.pendown()
+    jim.pencolor("black")
+    jim.backward(10)
+
+
+def turn_left():
+    jim.pendown()
+    jim.pencolor("black")
+    jim.left(45)
+
+
+def turn_right():
+    jim.pendown()
+    jim.pencolor("black")
+    jim.right(45)
+
+
+def clear_screen():
+    jim.clear()
+
+
+screen.listen() # starts the screen listening for events
+
+# sets onkey press event listener to call functions based on which key is pressed
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear_screen)
+
+screen.exitonclick()
