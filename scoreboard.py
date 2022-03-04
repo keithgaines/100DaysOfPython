4ba0c710367b0a2f0d01c9dabb0020935df1efea
keithgaines <93451842+keithgaines@users.noreply.github.com>
2022-03-04 08:53:17 -0600

    added scoreboard functionality

---
+++ scoreboard.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 15, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.pencolor("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 275)
+        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
+
+
+
+
