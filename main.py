ddb82d061223b919bef1126291c142cb9972e37a
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-03-07 10:42:25 -0600

    added calls to scoreboard class, and calls to the method to increase ball speed

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
+# sets up screen
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
+# initializes left and right paddles, ball, and scoreboard
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
+# game controls
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -21,8 +26,8 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
-    screen.update()
+    time.sleep(ball.move_speed)  # refresh rate for screen. lower number makes animations appear smoother
+    screen.update()  # updates screen after refresh rate period
     ball.move()
 
     # detect collision with top/bottom wall
@@ -33,6 +38,15 @@ while game_is_on:
     if ball.xcor() > 320 and ball.distance(r_paddle) < 50 or ball.xcor() < -320 and ball.distance(l_paddle) < 50:
         ball.bounce_x()
 
+    # detect right side miss - point for left side
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # detect left side miss - point for right side
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
 
