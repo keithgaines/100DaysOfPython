95840f455ffed405c31d6f8f620ab7d37980eaab
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-03-07 09:35:39 -0600

    cleaned up imports

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 import time
@@ -25,13 +25,13 @@ while game_is_on:
     screen.update()
     ball.move()
 
-    # detect collision with wall
+    # detect collision with top/bottom wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
     # detect collision with paddle
-    if ball.xcor() > 350 and r_paddle.distance(ball.xcor) < 50 or ball.xcor() < -350 and l_paddle.distance(ball.xcor) < 50:
-        ball.bounce()
+    if ball.xcor() > 320 and ball.distance(r_paddle) < 50 or ball.xcor() < -320 and ball.distance(l_paddle) < 50:
+        ball.bounce_x()
 
 
 screen.exitonclick()
