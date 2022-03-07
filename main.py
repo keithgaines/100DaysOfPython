ee08276f777ebc9a7103643cbce4de5d2b5f4d75
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-03-07 09:03:21 -0600

    created ball class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -18,7 +21,18 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with paddle
+    if ball.xcor() > 350 and r_paddle.distance(ball.xcor) < 50 or ball.xcor() < -350 and l_paddle.distance(ball.xcor) < 50:
+        ball.bounce()
+
 
 screen.exitonclick()
 
--- paddle.py
+++ paddle.py
@@ -1,13 +1,13 @@
 from turtle import Turtle
 
-class Paddle(Turtle)
+class Paddle(Turtle):
     def __init__(self, position):
-        super.__init__()
+        super().__init__()
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-        self.goto(350, 0)
+        self.goto(position)
 
     def go_up(self):
         new_y = self.ycor() + 20
