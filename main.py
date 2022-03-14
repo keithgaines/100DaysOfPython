e6fb59a0fa7fff2d50db16343526df2f2f9b7e7d
keithgaines <patrick.keith.gaines@gmail.com>
2022-03-14 09:36:11 -0500

    added turtle/car collision detection

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-frog = Player()
+turtle = Player()
 car_manager = CarManager()
 
 screen.listen()
-screen.onkey(frog.move_forward, "Up")
+screen.onkey(turtle.move_forward, "Up")
 
 game_is_on = True
 while game_is_on:
@@ -24,11 +24,11 @@ while game_is_on:
 
     # detect collision with car
     for car in car_manager.all_cars:
-        if car.distance(frog) < 20:
+        if car.distance(turtle) < 20:
             game_is_on = False
 
-    if frog.ycor() == 280:
-        frog.reset_position()
+    if turtle.ycor() == 280:
+        turtle.reset_position()
 
 
 
