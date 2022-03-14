ddb41ab9a8348e9ddb2e49d8b442f6b8a69a0919
keithgaines <patrick.keith.gaines@gmail.com>
2022-03-14 10:52:14 -0500

    added level up and increase car speed methods

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
 import time
-from turtle import Turtle
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
-# from scoreboard import Scoreboard
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-turtle = Player()
+player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(turtle.move_forward, "Up")
+screen.onkey(player.move_forward, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
     car_manager.create_car()
     car_manager.move_cars()
 
     # detect collision with car
     for car in car_manager.all_cars:
-        if car.distance(turtle) < 20:
+        if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
-    if turtle.ycor() == 280:
-        turtle.reset_position()
-
-
+    # detect successful crossing
+    if player.is_at_finish_line():
+        player.reset_position()
+        car_manager.level_up()
+        scoreboard.increase_level()
 
 screen.exitonclick()
