1692a2f6e598a466f843154ee6d7bf2df8ee97fb
keithgaines <patrick.keith.gaines@gmail.com>
2022-05-26 09:34:57 -0500

    calc comp score bug fix

--- Blackjack/.main.py
+++ Blackjack/.main.py
@@ -1,14 +1,14 @@
 ############### Our Blackjack House Rules #####################
 
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
+# The deck is unlimited in size.
+# There are no jokers.
+# The Jack/Queen/King all count as 10.
+# The Ace can count as 11 or 1.
+# Use the following list as the deck of cards:
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# The cards in the list have equal probability of being drawn.
+# Cards are not removed from the deck as they are drawn.
+# The computer is the dealer.
 
 import os
 import random
@@ -42,8 +42,8 @@ def compare(user_score, computer_score):
 
 def calculate_score(cards):
     """Take a list of cards and return the score calculated from the cards"""
-    if sum(cards) == 21 and len(cards) == 2:
-        return 0
+    # if sum(cards) == 21 and len(cards) == 2:
+    # return 0
     # if ace value of 11 is detected, and score is currently over 21, this removes the 11 value and replaces it with a 1
     if 11 in cards and sum(cards) > 21:
         cards.remove(11)
@@ -57,12 +57,13 @@ def play_game():
     computer_cards = []  # creates empty list called computer cards
     game_over = False
 
-    # loops through twice and adds the returned value of deal_card() to the end of the user_cards and computer_cards list
+    # loops through twice and adds the returned value of deal_card() to the end of the user_cards and computer_cards
+    # list
     for _ in range(2):
         user_cards.append(deal_card())
         computer_cards.append(deal_card())
 
-    while game_over == False:
+    while not game_over:
         user_score = sum(user_cards)
         computer_score = sum(computer_cards)
         print(f"Your cards: {user_cards}, current score: {user_score}")
@@ -79,7 +80,6 @@ def play_game():
                 print(f"Your final hand: {user_cards}, final score: {user_score}")
                 print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
                 print(compare(user_score, computer_score))
-                os.system('pause')
 
     # computer will continue to draw cards as long as the value of computer_cards is below 17
     while computer_score != 0 and computer_score < 17:
