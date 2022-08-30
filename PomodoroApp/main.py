c80251f5fef41b6d80e1e371ba844636c4ed6004
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-08-30 10:52:13 -0500

    changed count_sec to :00 format

--- PomodoroApp/main.py
+++ PomodoroApp/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -13,8 +14,19 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60) 
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count -1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -26,8 +38,9 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
+
 
 # Label 
 title = tk.Label()
@@ -36,7 +49,7 @@ title.grid(row=0, column=1)
 
 
 # Buttons
-start = tk.Button(text="Start", bg="white", highlightthickness=0)
+start = tk.Button(text="Start", bg="white", highlightthickness=0, command=start_timer)
 start.grid(row=2, column=0)
 
 reset = tk.Button(text="Reset", bg="white", highlightthickness=0)
