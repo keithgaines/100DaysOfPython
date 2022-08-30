efc23ae40780a77c88b7b4ac3a38dd1432fb6fed
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-08-30 08:18:36 -0500

    changed tkinter import

--- PomodoroApp/main.py
+++ PomodoroApp/main.py
@@ -1,4 +1,4 @@
-from tkinter import * 
+import tkinter as tk
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -18,12 +18,12 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-window = Tk()
+window = tk.Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="tomato.png")
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
