23fb719a238cfeaa8a405947ee098736f88158f8
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-08-30 09:17:44 -0500

    changed row/column numbers in UI

--- PomodoroApp/main.py
+++ PomodoroApp/main.py
@@ -27,25 +27,25 @@ canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(row=2, column=2)
+canvas.grid(row=1, column=1)
 
 # Label 
-label = tk.Label()
-label.config(text="Timer", font=("Times New Roman", 25, "bold"), bg=YELLOW, fg=GREEN)
-label.grid(row=1, column=2)
+title = tk.Label()
+title.config(text="Timer", font=("Times New Roman", 25, "bold"), bg=YELLOW, fg=GREEN)
+title.grid(row=0, column=1)
 
 
 # Buttons
 start = tk.Button(text="Start", bg="white", borderwidth=0)
-start.grid(row=3, column=1)
+start.grid(row=2, column=0)
 
 reset = tk.Button(text="Reset", bg="white", borderwidth=0)
-reset.grid(row=3, column=3)
+reset.grid(row=2, column=2)
 
 # checkmarks
 checkmark = tk.Label()
 checkmark.config(text="✔", bg=YELLOW, fg=GREEN, font=("Times New Roman", 25, "bold"))
 checkmark.config(pady=20)
-checkmark.grid(row=4, column=2)
+checkmark.grid(row=3, column=1)
 
 window.mainloop()
