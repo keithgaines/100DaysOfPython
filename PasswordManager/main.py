d3e35ed6369cb3293ca80cb97d2c25d51f1ef5b8
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-08-31 10:11:41 -0500

    added entry fields

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,4 +1,6 @@
+import email
 import tkinter as tk
+from turtle import width
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -15,14 +17,28 @@ logo = tk.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
 canvas.grid(row=0, column=1)
 
+# Labels
+
 website_label = tk.Label(text="Website:")
 website_label.grid(row=1, column=0)
 
 email_label = tk.Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
 
-password_label = tk.Label(text="Password:")
-password_label.grid(row=3, column=0)
+pw_label = tk.Label(text="Password:")
+pw_label.grid(row=3, column=0)
+
+# Entries
+
+website_input = tk.Entry(width=35)
+website_input.grid(row=1, column=1, columnspan=2)
+
+email_input = tk.Entry(width=35)
+email_input.grid(row=2, column=1, columnspan=2)
+
+pw_input = tk.Entry(width=21)
+pw_input.grid(row=3, column=1)
+
 
 
 window.mainloop()
