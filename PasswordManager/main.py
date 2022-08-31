769f55dc2862c4fc116e175d9a8c72f49c72cdea
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-08-31 10:59:23 -0500

    added buttons

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -10,7 +10,7 @@ from turtle import width
 
 window = tk.Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20, bg="white")
+window.config(padx=50, pady=50, bg="white")
 
 canvas = tk.Canvas(width=200, height=200, bg="white", highlightthickness=0)
 logo = tk.PhotoImage(file="logo.png")
@@ -19,26 +19,32 @@ canvas.grid(row=0, column=1)
 
 # Labels
 
-website_label = tk.Label(text="Website:")
+website_label = tk.Label(text="Website:", bg="white")
 website_label.grid(row=1, column=0)
 
-email_label = tk.Label(text="Email/Username:")
+email_label = tk.Label(text="Email/Username:", bg="white")
 email_label.grid(row=2, column=0)
 
-pw_label = tk.Label(text="Password:")
+pw_label = tk.Label(text="Password:", bg="white")
 pw_label.grid(row=3, column=0)
 
 # Entries
 
 website_input = tk.Entry(width=35)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input.grid(row=1, column=1, columnspan=2, sticky=tk.W)
 
 email_input = tk.Entry(width=35)
-email_input.grid(row=2, column=1, columnspan=2)
+email_input.grid(row=2, column=1, columnspan=2, sticky=tk.W)
 
-pw_input = tk.Entry(width=21)
-pw_input.grid(row=3, column=1)
+pw_input = tk.Entry()
+pw_input.grid(row=3, column=1, sticky=tk.W)
 
+# Buttons
 
+generate_button = tk.Button(text="Generate Password", bg="white")
+generate_button.grid(row=3, column=2, columnspan=2)
+
+add_button = tk.Button(text="Add", width=35, bg="white")
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
