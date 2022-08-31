1de07013bab640bbd404f4ebff2c1c3d4b4ee360
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-08-31 10:06:05 -0500

    added labels

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,5 +1,3 @@
-from curses import window
-from email.mime import image
 import tkinter as tk
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -15,6 +13,16 @@ window.config(padx=20, pady=20, bg="white")
 canvas = tk.Canvas(width=200, height=200, bg="white", highlightthickness=0)
 logo = tk.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+website_label = tk.Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+email_label = tk.Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+
+password_label = tk.Label(text="Password:")
+password_label.grid(row=3, column=0)
+
 
 window.mainloop()
