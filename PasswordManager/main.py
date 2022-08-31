c956091e744b697ca7628b56094c03c0bacbcf2c
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-08-31 09:49:15 -0500

    added logo

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,5 +1,20 @@
+from curses import window
+from email.mime import image
+import tkinter as tk
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = tk.Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20, bg="white")
+
+canvas = tk.Canvas(width=200, height=200, bg="white", highlightthickness=0)
+logo = tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.pack()
+
+window.mainloop()
