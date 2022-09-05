ee18875d5faf712fe6680f0387d9b5988b86b67b
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-09-05 14:42:13 -0500

    added remove card once learned functionality

--- flashCardProject/main.py
+++ flashCardProject/main.py
@@ -1,28 +1,65 @@
-BACKGROUND_COLOR = "#B1DDC6"
-
-from dataclasses import field
-from email.mime import image
 import tkinter as tk
-from turtle import width
+import pandas as pd
+import random
 
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+try: 
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front)
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back)
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pd.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    
+    next_card()
+
+# ---------- UI SETUP ---------- # 
 window = tk.Tk()
-window.config(bg=BACKGROUND_COLOR)
 window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = tk.Canvas(height=526, width=800, highlightthickness=0)
-card_front = tk.PhotoImage(file="./images/card_front.png")
-canvas.create_image(526, 800, image=card_front)
-title = canvas.create_text(400, 150, text="Title", font=("arial", 40, "italic"))
-word = canvas.create_text(400, 263, text="word", font=("arial", 60, "bold"))
+flip_timer = window.after(3000, func=flip_card)
 
-canvas.grid(padx=50, pady=50, column=0, columnspan=2)
+canvas = tk.Canvas(width=800, height=526)
+card_front = tk.PhotoImage(file="images/card_front.png")
+card_back = tk.PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front)
+card_title = canvas.create_text(400, 150, text="", font=("arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("arial", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(column=0, columnspan=2)
 
 x_image = tk.PhotoImage(file="./images/wrong.png")
-x_button = tk.Button(image=x_image, highlightthickness=0)
-x_button.grid(column=0, row=1)
+x_button = tk.Button(image=x_image, highlightthickness=0, command=next_card)
+x_button.grid(row=1, column=0)
 
 check_image = tk.PhotoImage(file="./images/right.png")
-check_button = tk.Button(image=check_image, highlightthickness=0)
-check_button.grid(column=1, row=1)
+check_button = tk.Button(image=check_image, highlightthickness=0, command=is_known)
+check_button.grid(row=1, column=1)
 
+
+next_card()
 window.mainloop()
