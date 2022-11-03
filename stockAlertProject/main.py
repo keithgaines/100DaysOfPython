ba9231abc0e55397c58ebc0c333d6990aa39898b
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-11-03 10:39:23 -0500

    calculated percentage difference between 2 days

--- stockAlertProject/main.py
+++ stockAlertProject/main.py
@@ -1,11 +1,9 @@
 import requests
-import json
-import math
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-STOCK_ENDPOINT = "https://www.alphavantage.co/query/?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=YC0TAL7I2G3NHWBL"
+STOCK_ENDPOINT = "https://www.alphavantage.co/query/?function=TIME_SERIES_DAILY&symbol=TSLA&apikey={your_API_key}"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
@@ -18,17 +16,17 @@ price = data.json()
 
 dailyprices = {key:value for (key, value) in price.items()}
 
-firstclosingprice = dailyprices['Time Series (Daily)']['2022-11-02']['4. close']
+firstclosingprice = float(dailyprices['Time Series (Daily)']['2022-11-02']['4. close'])
 print("Nov. 2nd closing price was: ", firstclosingprice) # dailyprices['Time Series (Daily)']['2022-11-02']['4. close'])
     # print(dailyprices['4. close'])
 
 #TODO 2. - Get the day before yesterday's closing stock price
-secondclosingprice = dailyprices['Time Series (Daily)']['2022-11-01']['4. close']
+secondclosingprice = float(dailyprices['Time Series (Daily)']['2022-11-01']['4. close'])
 print("Nov. 1st closing price was: ", secondclosingprice)
 
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 
-difference = float(firstclosingprice) - float(secondclosingprice)
+difference = firstclosingprice - secondclosingprice
 difference = round(difference)
 
 if difference < 0:
@@ -39,33 +37,6 @@ print('\nThe positive difference is: ', difference)
 
 #TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+percentagedifference = difference / ((firstclosingprice + secondclosingprice) / 2) * 100
 
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
-
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+print('\nThe percentage difference is: ', percentagedifference)
