268dbb5aaec103f4714fe82937d4aa939f62e33a
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-11-03 10:09:01 -0500

    printed previous 2 days stock prices and calculated the difference

--- stockAlertProject/main.py
+++ stockAlertProject/main.py
@@ -1,7 +1,11 @@
+import requests
+import json
+import math
+
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+STOCK_ENDPOINT = "https://www.alphavantage.co/query/?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=YC0TAL7I2G3NHWBL"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
@@ -9,9 +13,29 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 #TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
 
+data = requests.get(STOCK_ENDPOINT)
+price = data.json()
+
+dailyprices = {key:value for (key, value) in price.items()}
+
+firstclosingprice = dailyprices['Time Series (Daily)']['2022-11-02']['4. close']
+print("Nov. 2nd closing price was: ", firstclosingprice) # dailyprices['Time Series (Daily)']['2022-11-02']['4. close'])
+    # print(dailyprices['4. close'])
+
 #TODO 2. - Get the day before yesterday's closing stock price
+secondclosingprice = dailyprices['Time Series (Daily)']['2022-11-01']['4. close']
+print("Nov. 1st closing price was: ", secondclosingprice)
 
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+
+difference = float(firstclosingprice) - float(secondclosingprice)
+difference = round(difference)
+
+if difference < 0:
+    difference = difference * -1
+else:
+    pass
+print('\nThe positive difference is: ', difference)
 
 #TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
 
