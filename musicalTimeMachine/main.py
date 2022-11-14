431264eeaa5a3b7cb82e894c21d354bb40362860
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-11-13 19:23:08 -0600

    scraped song titles into list

--- musicalTimeMachine/main.py
+++ musicalTimeMachine/main.py
@@ -5,4 +5,12 @@ import pandas
 date = input('Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ')
 
 
-URL = ''
+URL = f'https://www.billboard.com/charts/hot-100/{date}'
+
+response = requests.get(URL)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+song_names = soup.find_all(name="h3", id="title-of-a-story", class_="a-no-trucate")
+song_names = [song.get_text().strip('\n\t') for song in song_names]
+
+print(song_names)
