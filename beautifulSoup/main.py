65a6dd45f5e1fe585494ec487813692125189f60
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-11-13 18:28:01 -0600

    imported pandas to convert data framme to csv

--- beautifulSoup/main.py
+++ beautifulSoup/main.py
@@ -1,7 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
 response = requests.get(URL)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+all_h3 = soup.find_all(name='h3')
+movies_ranked = []
+for tag in all_h3:
+    tag = tag.getText()
+    movies_ranked.insert(0, tag)
+
+df = pandas.DataFrame(data={'Top 100 Movies of All Time': movies_ranked})
+df.to_csv('ranked movies list.csv', sep=',', index=False)
